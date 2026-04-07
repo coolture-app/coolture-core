3f0efcb4d7920344679f6a4aac1ca32bbf319714
Stanisław Bożych <stachubozych@gmail.com>
2026-04-07 14:17:19 +0200

    fix security config

--- gateway/src/main/java/pl/coolture/gateway/config/SecurityConfig.java
+++ gateway/src/main/java/pl/coolture/gateway/config/SecurityConfig.java
@@ -22,6 +22,9 @@ public class SecurityConfig {
   @Value("${app.allowed-origins:http://localhost:3900,http://localhost:8080,http://localhost:8180,http://localhost:8081,http://localhost:4200}")
   private String allowedOrigins;
 
+    @Value("${app.frontend.redirect-uri:http://localhost:4200/}")
+    private String frontendRedirectUri;
+
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) {
     http.authorizeHttpRequests(
@@ -30,10 +33,23 @@ public class SecurityConfig {
                     .permitAll()
                     .anyRequest()
                     .authenticated())
-        .oauth2Login(Customizer.withDefaults())
+        .oauth2Login(oauth2 -> oauth2
+            .successHandler((request, response, authentication) -> {
+                response.sendRedirect(frontendRedirectUri);
+            })
+        )
         .oauth2Client(Customizer.withDefaults())
         .csrf(AbstractHttpConfigurer::disable)
-        .cors(cors -> cors.configurationSource(corsConfigurationSource()));
+        .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+        .exceptionHandling(exceptions -> exceptions
+                  .authenticationEntryPoint((request, response, authException) -> {
+                      if (request.getRequestURI().startsWith("/api/")) {
+                          response.setStatus(HttpStatus.UNAUTHORIZED.value());
+                      } else {
+                          response.sendRedirect("/oauth2/authorization/keycloak");
+                      }
+                  })
+          );
 
     return http.build();
   }
