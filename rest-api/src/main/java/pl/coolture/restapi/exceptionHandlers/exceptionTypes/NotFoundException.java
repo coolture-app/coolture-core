e488e0856136d947fc813637517cf85059932971
Kacper Iterman <kacperiterman@proton.me>
2026-04-24 11:10:16 +0000

    chore: remove old classes, nothing depended on them anymore

--- rest-api/src/main/java/pl/coolture/restapi/exceptionHandlers/exceptionTypes/NotFoundException.java
+++
@@ -1,9 +0,0 @@
-package pl.coolture.restapi.exceptionHandlers.exceptionTypes;
-
-import org.springframework.http.HttpStatus;
-
-public class NotFoundException extends ApiException {
-  public NotFoundException(String message) {
-    super(message, HttpStatus.NOT_FOUND, "Not Found");
-  }
-}
--- rest-api/src/main/java/pl/coolture/restapi/utils/InteractionChangeInfo.java
+++
@@ -1,3 +0,0 @@
-package pl.coolture.restapi.utils;
-
-public record InteractionChangeInfo(boolean changeLike, boolean changeParticipating) {}
