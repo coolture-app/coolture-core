e94f92e190717f149e8f14ae2d8e6923502c9727
KrzysKond <gaxkk19@gmail.com>
2026-05-22 00:00:41 +0200

    remove categories

--- rest-api/src/test/java/pl/coolture/restapi/dictionary/DictionaryAdminControllerTest.java
+++ rest-api/src/test/java/pl/coolture/restapi/dictionary/DictionaryAdminControllerTest.java
@@ -7,7 +7,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.util.UUID;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -17,10 +16,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import pl.coolture.restapi.common.security.ControllerTestWithSecurity;
 import pl.coolture.restapi.dictionary.api.DictionaryAdminController;
 import pl.coolture.restapi.dictionary.api.dto.CountryCodeDto;
-import pl.coolture.restapi.dictionary.api.dto.EventCategoryDto;
-import pl.coolture.restapi.dictionary.application.DictionaryAdminService;
 import pl.coolture.restapi.dictionary.api.dto.CountryCodeRequest;
-import pl.coolture.restapi.dictionary.api.dto.EventCategoryRequest;
+import pl.coolture.restapi.dictionary.application.DictionaryAdminService;
 import pl.coolture.restapi.user.application.UserProvisioningService;
 
 /**
@@ -40,45 +37,12 @@ class DictionaryAdminControllerTest {
     @MockitoBean DictionaryAdminService adminService;
     @MockitoBean UserProvisioningService userProvisioningService;
 
-    private static final String CATEGORIES = "/dicts/admin/event-categories";
     private static final String COUNTRY_CODES = "/dicts/admin/country-codes";
 
     private static final SimpleGrantedAuthority ADMIN =
             new SimpleGrantedAuthority("ROLE_COOLTURE_ADMIN");
     private static final SimpleGrantedAuthority USER =
             new SimpleGrantedAuthority("ROLE_COOLTURE_USER");
-
-    @Test
-    void createCategory_returns401_whenAnonymous() throws Exception {
-        mockMvc.perform(post(CATEGORIES)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content("{\"name\":\"concert\"}"))
-                .andExpect(status().isUnauthorized());
-    }
-
-    @Test
-    void createCategory_returns403_whenAuthenticatedWithoutAdmin() throws Exception {
-        mockMvc.perform(post(CATEGORIES)
-                        .with(jwt().authorities(USER))
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content("{\"name\":\"concert\"}"))
-                .andExpect(status().isForbidden());
-    }
-
-    @Test
-    void createCategory_returns201_withAdminRole() throws Exception {
-        UUID id = UUID.randomUUID();
-        when(adminService.createCategory(any(EventCategoryRequest.class)))
-                .thenReturn(new EventCategoryDto(id, "concert"));
-
-        mockMvc.perform(post(CATEGORIES)
-                        .with(jwt().authorities(ADMIN))
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content("{\"name\":\"concert\"}"))
-                .andExpect(status().isCreated())
-                .andExpect(jsonPath("$.id").value(id.toString()))
-                .andExpect(jsonPath("$.name").value("concert"));
-    }
 
     @Test
     void createCountryCode_returns401_whenAnonymous() throws Exception {
